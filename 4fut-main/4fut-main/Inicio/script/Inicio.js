f340362728b88fc686e6228b5997a437ceee5a40
Joao Vitor Pereira Ferreira <joaoferreira@aluno.senai.br>
2023-10-19 16:21:59 -0300

    progresso com a Api

--- 4fut-main/4fut-main/Inicio/script/Inicio.js
+++ 4fut-main/4fut-main/Inicio/script/Inicio.js
@@ -21,29 +21,57 @@ function MenuBurguer() {
   menu.classList.toggle("Desativado")
 }
 
-window.addEventListener('load', function(){
-  new Glider(document.querySelector('.glider'), {
-    setting-name: setting-value
+// window.addEventListener('load', function(){
+//   new Glider(document.querySelector('.glider'), {
+//     setting-name: setting-value
+//   })
+// })
+
+
+
+
+fetch(' https://newsapi.org/v2/everything?q=futebol&apiKey=a2381a88d1f04fc89754c87f3d3d8c29')
+.then(response => response.json())
+.then(data => {
+  // Acesse o título e a URL da imagem dos dados recebidos da API
+  const titulo = data.articles[0].title;
+  const imagemURL = data.articles[0].urlToImage;
+
+  // Selecione os elementos HTML onde deseja exibir o título e a imagem
+  const tituloElement = document.getElementById('TituloN1');
+  const imagemElement = document.getElementById('ImagemN1');
+
+  // Atualize o conteúdo desses elementos com os dados da API
+  tituloElement.textContent = titulo;
+  imagemElement.src = imagemURL;
+
+ 
   })
-})
+ .catch(error => {
+    console.error('Erro ao buscar os dados da API:', error);
+  });
 
-
-
+  
 
 // fetch(' https://newsapi.org/v2/everything?q=keyword&apiKey=a2381a88d1f04fc89754c87f3d3d8c29')
-//   .then(response => response.json())
-//   .then(data => {
-    
-    
-//     // Manipule os dados recebidos da API aqui
-//     // Por exemplo, se os dados são um objeto com um campo "texto", você pode fazer algo assim:
-//     const texto = data.texto;
+// .then(response => response.json())
+// .then(data => {
+//   // Acesse o título e a URL da imagem dos dados recebidos da API
+//   const titulo = data.articles[0].title;
+//   const imagemURL = data.articles[0].urlToImage;
 
-//     // Selecione o elemento HTML onde deseja exibir os dados
-//     const conteudoElement = document.getElementById('conteudo');
+//   // Selecione os elementos HTML onde deseja exibir o título e a imagem
+//   const tituloElement = document.getElementById('TituloN1');
+//   const imagemElement = document.getElementById('imgN1');
 
-    
+//   // Atualize o conteúdo desses elementos com os dados da API
+//   tituloElement.textContent = titulo;
+//   imagemElement.src = imagemURL;
 
-//     // Atualize o conteúdo desse elemento com os dados da API
-//     conteudoElement.textContent = texto;
+
+//   imagemElement.setAttribute('src', imagemURL)
+ 
+//   })
+//   .catch(error => {
+//     console.error('Erro ao buscar os dados da API:', error);
 //   });
